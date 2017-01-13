54cea2ee7158ff07d4709dd363d1b8a63db881f8
Nico Peter <nico.peter@kit.edu>
2017-01-13 12:48:29 +0100

    First Spark job works but still get exception for winutils

--- src/main/java/org/kit/energy/SparkModel.java
+++ src/main/java/org/kit/energy/SparkModel.java
@@ -1,6 +1,8 @@
 package org.kit.energy;
 
-import org.springframework.context.annotation.Scope;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,5 +12,19 @@ import org.springframework.stereotype.Component;
 public class SparkModel {
     public void doStuff(){
         System.out.println("I do stuff, dough!");
+        String logFile = "README.txt";
+        SparkConf conf = new SparkConf().setAppName("simple app")
+                .setMaster("local").set("spark.executor.memory","1g");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        long numAs = logData.filter(s -> s.contains("a")).count();
+        long numBs = logData.filter(s -> s.contains("b")).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        sc.stop();
     }
+
 }
+
